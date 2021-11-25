946e7936a342bcb87c58f9e520badf1b73548005
alanj <alanj@DESKTOP-FD3UE7D>
2021-11-25 05:57:08 +0000

    Login Page Connection Done

--- Musk Assisgnment/Log In Page.cs
+++ Musk Assisgnment/Log In Page.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Musk_Assisgnment
 {
@@ -44,26 +45,36 @@ namespace Musk_Assisgnment
 
         private void LogIn_Button(object sender, EventArgs e)
         {
-            Dashboard newdashboard = new Dashboard();
-            this.Hide();
-            newdashboard.ShowDialog();
-            this.Show();
-            //(new Dashboard()).Show();
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=Z:\Musk Assignment\Musk Assisgnment\Database1.mdf;Integrated Security=True");
+            SqlDataAdapter sda = new SqlDataAdapter("select count(*) from login where username = '" + usernameTextBox.Text + "' and password= '" + passwordTextBox.Text + "'", conn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "i")
+            {
+                this.Hide();
+                Dashboard mm = new Dashboard();
+                this.Show();
+                //(new Dashboard()).Show(); 
+            }
+            else
+            {
+                MessageBox.Show("Please Enter correct username or password", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+            private void SignUp_Button(object sender, EventArgs e)
+            {
 
-        private void SignUp_Button(object sender, EventArgs e)
-        {
+            }
 
-        }
+            private void ForgotPassword_labelLink(object sender, EventArgs e)
+            {
 
-        private void ForgotPassword_labelLink(object sender, EventArgs e)
-        {
+            }
 
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+            private void button4_Click(object sender, EventArgs e)
+            {
+                this.Close();
+            }
+        
     }
 }
